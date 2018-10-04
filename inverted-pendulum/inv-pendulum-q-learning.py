7c52c69590730a517ee934f7a72c075b075e1f89
robertofem <robertofem@gmail.com>
2018-10-04 02:19:48 +0200

    try to make inverted pendulum to work, it does not

--- inverted-pendulum/inv-pendulum-q-learning.py
+++ inverted-pendulum/inv-pendulum-q-learning.py
@@ -7,12 +7,12 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
 
-EPISODES = 1000
+EPISODES = 5000
 
-GANMA = 0.95    # discount rate
+GANMA = 0.99    # discount rate
 EPSILON = 1.0  # exploration rate
 EPSILON_MIN = 0.01
-EPSILON_DECAY = 0.995
+EPSILON_DECAY = 0.999
 LEARNING_RATE = 0.001
 
 #TRAIN = 1 trains to a file. TRAIN = 0 plays with the coefficients of the file.
@@ -27,10 +27,10 @@ if __name__ == "__main__":
   #Create the artificial neural network (ann)
   ann = Sequential()
 
-  ann.add(Dense(10, input_dim = state_size, activation='sigmoid'))
-  ##ann.add(Dense(10, activation='sigmoid'))
+  ann.add(Dense(64, input_dim = state_size, activation='relu'))
+  #ann.add(Dense(64, activation='relu'))
   ann.add(Dense(2, activation='linear'))
-  ann.compile(loss='mse', optimizer='adam', metrics=['mae'])
+  ann.compile(loss='mse', optimizer=Adam(LEARNING_RATE), metrics=['mae'])
 
   if TRAIN == 1:
     epsilon = EPSILON
@@ -41,26 +41,33 @@ if __name__ == "__main__":
       epsilon *= EPSILON_DECAY;
       done = False
       time = 0
+      #choose greedy action
+      action = np.argmax(ann.predict(state)[0])
       while not done:
-        env.render()
-        # Select an action (epsilon-greedy)
-        if np.random.random() < epsilon:
-            action = random.randrange(action_size)
-        else:
-            act_values = ann.predict(state)
-            action = np.argmax(act_values[0])
+        # env.render()
         # Take that action and see the next state and reward
         new_state, reward, done, _ = env.step(action)
         new_state = np.reshape(new_state, [1, state_size])
+        if done:
+          target = reward
+          target_vec = ann.predict(state)
+          target_vec[0][action] = target
+          ann.fit(state, target_vec, epochs=1, verbose=0)
+          break
+        # Select an new action (epsilon-greedy)
+        if np.random.random() < epsilon:
+            new_action = random.randrange(action_size)
+        else:
+            new_action = np.argmax(ann.predict(new_state)[0])
         # Train the model if the episode finished
-        target = reward + GANMA * np.max(ann.predict(new_state))
+        target = reward + GANMA * np.argmax(ann.predict(new_state)[0])#[new_action]
         target_vec = ann.predict(state)
         target_vec[0][action] = target
-        ann.fit(state, target_vec.reshape(-1, 2), epochs=1, verbose=0)
+        ann.fit(state, target_vec, epochs=1, verbose=0)
         state = new_state
+        action = new_action
         # Count the time
         time += 1
-
 
       #print episode results
       print("episode: {}/{}, score: {}, e: {:.2}"
